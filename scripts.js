4e1e3667b60468b6888c3b515694582473424fe3
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-09-16 14:22:54 -0400

    added for loops to display images, price and description on the page

--- scripts.js
+++ scripts.js
@@ -1,19 +1,27 @@
 import data from './data.js'
 
+//define Variables
 const itemsContainer = document.querySelector('#items');
 
 
 
-    //produces images on the page
-    for (let i = 0; i < data.length; i += 1) {
-        const newDiv = document.createElement('div');
-        newDiv.className = 'item'
-        const img = document.createElement('img');
-        img.src = data[i].image;
-        img.width = 300;
-        img.height = 300;
-        newDiv.appendChild(img);
-        itemsContainer.appendChild(newDiv);
-    }
+//produces images on the page
+for(let i = 0; i <data.length; i += 1){
+    const newDiv = document.createElement('div'); // <div></div>
+    newDiv.className = 'item'; //  <div class = 'item' ></div>
+    const img = document.createElement('img'); //<img>
+    img.src = data[i].image; //links image source to image in data.js
+    img.width = 300;
+    img.height = 300;
+    newDiv.appendChild(img); // adds img to new Div
+    //create P element for Description
+    const description = document.createElement ('P');
+    description.innerText= data[i].desc; 
+    newDiv.append(description);
+    //Create P element for Price
+    const price = document.createElement('P');
+    price.innerText = data[i].price;
+    newDiv.appendChild(price);
+    itemsContainer.appendChild(newDiv); //adds newDiv to item container in html  
+}
 
-    
