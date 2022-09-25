e5ac185995950c8a79cddfeeacea396095bc783f
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-09-25 13:53:37 -0400

    saving progress

--- scripts.js
+++ scripts.js
@@ -3,9 +3,27 @@ import data from './data.js'
 //define Variables
 const itemsContainer = document.querySelector('#items');
 const itemList = document.getElementById('item-list');
-const itemMessage = document.getElementById('item-message');
+const cartQty = document.getElementById('cart-qty');
 const cartTotal =document.getElementById('cart-total');
+const cartButtons = Array.from(document.querySelectorAll("button"))
 
+console.log(cartButtons);
+
+//connecting the add to cart button to the shopping cart
+// cartButtons.forEach(e => e.addEventListener('click', () => {
+//     addToCart(e.getAttribute('name'), e.getAttribute('price'))
+//     showCartItems()
+//   }));
+
+  cartButtons.forEach(elt => elt.addEventListener('click', () =>{
+    console.log('Button clicked')
+  }))
+
+//   all_items_button.forEach( e => e.addEventListener('click', () => {
+//     addToCart(e.getAttribute('ied'), e.getAttribute('data-price'))
+//     showCartItems()
+//   }))
+ 
 
 
 //produces images on the page
@@ -25,6 +43,7 @@ for(let i = 0; i <data.length; i += 1){
     const price = document.createElement('P');
     price.innerText = data[i].price;
     newDiv.appendChild(price);
+    //button
     const button = document.createElement('button'); // Make a button 
     button.id = data[i].name; //add an  id name to the button
     button.dataset.price = data[i].price //creates a custom attribute called data-price.
@@ -60,13 +79,14 @@ function addToCart(name, price) {
 //show items
 function showCartItems() {
     const qty = getQty(); //calls function and returns total at the same time
-    itemMessage.innerHTML = `You have ${qty} items in your cart`;
+    cartQty.innerHTML = `You have ${qty} items in your cart`;
 
     let itemStr = ' '
     for(let i =0; i < cart.length; i += 1) {
-        itemStr += `<li> ${cart[i].name}, $${cart[i].price} x ${cart[i].qty} =  $${cart[i].price * cart[i].qty} </li>`;
+        itemStr += `<li> ${cart[i].name}
+         $${cart[i].price} x ${cart[i].qty} =  
+         $${cart[i].price * cart[i].qty} </li>`;
     }
-
     itemList.innerHTML = itemStr;
     let total = calculateTotal();
     cartTotal.innerHTML = `Your cart total is ${total}`
@@ -112,12 +132,5 @@ function removeItem(name, qty = 0){
 }
 
 
-addToCart ('apple', 7.00);
-addToCart ('oranges', 6.55);
-addToCart ('apple', 7.00);
-addToCart ('oranges', 6.55);
-addToCart ('frisbee', 4.00);
-addToCart ('oranges', 6.55);
+
 showCartItems();
-removeItem('apple', 2)
-showCartItems()
